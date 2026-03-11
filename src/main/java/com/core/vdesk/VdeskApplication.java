8ada338c08142d1fce3961cca662fbe9099179a4
ggambo <ggambopce@gmail.com>
2026-03-11 12:46:00 +0900

    [FEAT] 패키지 구조 작성

---
+++ src/main/java/com/core/vdesk/VdeskApplication.java
@@ -0,0 +1,13 @@
+package com.core.vdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdeskApplication.class, args);
+	}
+
+}
